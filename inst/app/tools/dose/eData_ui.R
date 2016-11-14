8b1fbd98f282b0f721dc1dbef73c3bd64dfa06b9
kmezhoud <kmezhoud@gmail.com>
2016-11-14 22:44:12 +0100

    evars position

--- inst/app/tools/dose/eData_ui.R
+++ inst/app/tools/dose/eData_ui.R
@@ -74,37 +74,26 @@ output$dataEdit <- DT::renderDataTable({
   if (inherits(try( dat <- r_data[[input$dataset]][input$edata_vars], silent=TRUE),"try-error")){
     dat <- r_data[[input$dataset]]
   }else{
-  dat <- r_data[[input$dataset]][input$edata_vars]
-  #dat <- select_(.getdata(), .dots = input$edata_vars)
-}
-  ## update state when view_vars changes
-  # if (!identical(r_state$data_vars, input$data_vars)) {
-  #   r_state$data_vars <<- input$data_vars
-  #   r_state$data_state <<- list()
-  #   #r_state$dataviewer_search_columns <<- rep("", ncol(dat))
-  # }
-   displayTable(dat, session=session)
-  # search <- r_state$data_state$search$search
-  # if (is.null(search)) search <- ""
-  # fbox <- if (nrow(dat) > 5e6) "none" else list(position = "top")
-  #
-  # withProgress(message = 'Generating view table', value = 1,
-  #              DT::datatable(dat, filter = fbox, selection = "none",
-  #                            rownames = FALSE, style = "bootstrap", escape = FALSE,
-  #                            options = list(
-  #                              stateSave = TRUE,   ## maintains state
-  #                              searchCols = lapply(r_state$dataviewer_search_columns, function(x) list(search = x)),
-  #                              search = list(search = search, regex = TRUE),
-  #                              order = {if (is.null(r_state$data_state$order)) list()
-  #                                else r_state$data_state$order},
-  #                              columnDefs = list(list(orderSequence = c("desc", "asc"), targets = "_all"),
-  #                                                list(className = "dt-center", targets = "_all")),
-  #                              autoWidth = TRUE,
-  #                              processing = FALSE,
-  #                              pageLength = {if (is.null(r_state$data_state$length)) 10 else r_state$data_state$length},
-  #                              lengthMenu = list(c(5, 10, 25, 50, -1), c("5", "10","25","50","All"))
-  #                            ),
-  #                            callback = DT::JS("$(window).unload(function() { table.state.clear(); })")
-  #              )
-  # )
+    dat <- r_data[[input$dataset]][input$edata_vars]
+    #dat <- select_(.getdata(), .dots = input$edata_vars)
+  }
+
+  displayTable(dat, session=session)
+
+})
+
+output$editable <- renderUI({
+  tagList(
+  wellPanel(
+    lapply(1:length(input$edata_vars), function(i) {
+      div(style="display:inline-block",
+          h5("Accuracy table:"),
+          textInput(paste0("input",i), paste0('labal1',i),value = 2,width = "80%")
+      )}),
+    actionButton("submit", "Submit"),
+    actionButton("reset", "Reset")
+  ),
+
+  DT::dataTableOutput("dataEdit")
+)
 })
