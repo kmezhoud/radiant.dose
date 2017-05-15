f5ae53dea4413d810b0e6f8a1943f959af8c1b96
kmezhoud <kmezhoud@gmail.com>
2017-05-15 19:51:10 +0100

    selectize medical report

--- inst/app/tools/dose/medical.R
+++ inst/app/tools/dose/medical.R
@@ -1,11 +1,4 @@
 
-############
-
-
-
-############
-
-
 #### begin display image from file
 output$files <- renderTable(input$files)
 
@@ -22,12 +15,13 @@ output$images <- renderUI({
            function(i)
            {
              imagename = paste0("image", i)
-             imageOutput(imagename)
+             imageOutput(imagename, width = input$zoom_MedicalReport)
            })
 
   do.call(tagList, image_output_list)
 })
 
+## display  Images dowloaded from file
 observe({
   for (i in seq_along(nrow(files())))
   {
@@ -43,4 +37,59 @@ observe({
   }
 })
 
+
 #### end display image from file
+
+### begin display path
+observeEvent(
+  ignoreNULL = TRUE,
+  eventExpr = {
+    input$directory
+  },
+  handlerExpr = {
+    if (input$directory > 0) {
+      # condition prevents handler execution on initial app launch
+
+      path = choose.dir(default = readDirectoryInput(session, 'directory'))
+      updateDirectoryInput(session, 'directory', value = path)
+    }
+  }
+)
+
+# output$directory = renderText({
+#   readDirectoryInput(session, 'directory')
+# })
+#
+# output$filesPath = renderDataTable({
+#   files = list.files(readDirectoryInput(session, 'directory'), full.names = TRUE)
+#   data.frame(name = basename(files), file.info(files))
+# })
+
+## End display path
+
+## download Image from file
+# output$downloadImage <- downloadHandler(
+#   filename = "Shinyplot.png",
+#   content = function(file) {
+#     png(file)
+#
+#     dev.off()
+# })
+
+
+
+observeEvent(input$medical_report, {
+
+  cmd4 <- paste0("```{r}\n",
+                 "library(knitr)\n",
+                 "image_file <- paste(paste0(readDirectoryInput(session, 'directory'), input$periodeId_Med, sep=''),
+                                     '/', input$reports_Med ,sep='')\n",
+                 #"shiny::img(src= image_file,  width = 360 )",
+                 " obj <- lapply(image_file, function(x){shiny::img(src=x,  width = 360)})\n",
+                 "shiny::img(obj) \n",
+                 "\n```\n")
+
+
+  update_report_fun(cmd4)
+
+})
--- inst/app/tools/dose/medical_ui.R
+++ inst/app/tools/dose/medical_ui.R
@@ -58,18 +58,18 @@ output$ui_name_Medical <- renderUI({
 
   if(is.null(input$split_Medical) || input$split_Medical == 'All'){
 
-   }else{
-   dat <- dat[which(dat$Department == input$split_Medical),]
-   }
+  }else{
+    dat <- dat[which(dat$Department == input$split_Medical),]
+  }
   ## collapse Name and first name
   dat$x <- apply( dat[ , c('first.Name', 'Name') ] , 1 , paste , collapse = " " )
 
   choice <- unique(dat$x)
 
   selectInput(inputId = "name_Med", label= "Names:", choices = choice,
-              selected =  state_multiple("name_Med", choice),
-              multiple =FALSE, size = min(3, length(choice)), selectize = FALSE
-              )
+              selected =  choice[1],  #state_multiple("name_Med", choice),
+              multiple =TRUE, size = min(3, length(choice)), selectize = FALSE
+  )
 
 })
 
@@ -77,32 +77,56 @@ output$ui_name_Medical <- renderUI({
 
 
 display_Medical_report <- function(){
-  report_Path <- paste0("../extdata/reports/", input$periodeId_Med, sep="")
+  report_Path <- paste0( readDirectoryInput(session, 'directory'), input$periodeId_Med, sep="")
 
-  personal_reports <- as.list(list.files(report_Path)[grep(input$name_Med, list.files(report_Path))])
+  #report_Path <- paste0("../extdata/reports/", input$periodeId_Med, sep="")
 
- return(personal_reports)
+  personal_reports <- as.list(list.files(report_Path)[grep(paste0(input$name_Med,collapse = "|"), # collapse "|" usefull if multiple name_Med is selected
+                                                           list.files(report_Path),
+                                                           ignore.case = TRUE)
+                                                      ])
+
+
+  return(personal_reports)
 
 }
 
 
+output$ui_Medical_reports <- renderUI({
 
-lapply(1:3, function(i){
-output[[paste0('display_image',i)]] <- renderImage({
 
-  #print(display_Medical_report()[i])
+  choice <- display_Medical_report()
 
-  image_file <- paste(paste0("../extdata/reports/", input$periodeId_Med, sep=""),
-                      "/", display_Medical_report()[i] ,sep="")
+  selectInput(inputId = "reports_Med", label= "Report files:", choices = choice,
+              selected =  choice[1],  #state_multiple("name_Med", choice),
+              multiple =TRUE, size = min(3, length(choice)), selectize = FALSE)
+})
 
-  return(list(
-    src = image_file,
-    filetype = "image/jpg",
-    height = "1000%",
-    width = "100%"
-  ))
 
-}, deleteFile = FALSE)
+## render multiple Image using lapply
+
+lapply(1:6, function(i){
+  output[[paste0('display_image',i)]] <- renderImage({
+
+    #print(length(display_Medical_report()))
+
+    ## without selecting report files
+    #image_file <- paste(paste0(readDirectoryInput(session, 'directory'), input$periodeId_Med, sep=""),
+     #                   "/", display_Medical_report()[i] ,sep="")
+
+    ##  with selecting report file input$reports_Med from sidebar menu
+    image_file <- paste(paste0(readDirectoryInput(session, 'directory'), input$periodeId_Med, sep=""),
+                                           "/", input$reports_Med[i] ,sep="")
+
+
+    return(list(
+      src = image_file,
+      filetype = "image/jpg",
+      height = "1000%",
+      width = "100%"
+    ))
+
+  }, deleteFile = FALSE)
 
 })
 
@@ -111,53 +135,95 @@ output$medical <- renderUI({
 
   sidebarLayout(
 
-####  Sidebar Panels ###
+    ####  Sidebar Panels ###
     sidebarPanel(
       #uiOutput("ui_dataset_Medical"),
       uiOutput("ui_which_dataset_med"),
+
+      wellPanel(
+        ## set Path directory. default ~  or ../extdata/reports/
+        directoryInput('directory', label = 'Selected medical report directory',
+                       value = '../extdata/reports/'),
+
+        h5(paste0("Check box to view medical report"), align="left", style = "color:red"),
+
+
+          div(class="row",
+              div(class="col-xs-4",
+        tagList(
+        checkboxInput("view_MedReportID", label="")
+        )
+        ),
+        div(class="col-xs-5",
+        tagList(
+        icon = icon("arrow-left")
+        )
+        )
+        )
+
+        #actionButton("view_MedReportID", "Check box to view medical report",   icon=icon("arrow-right"))
+      ),
+
       uiOutput("ui_periode_Medical"),
       uiOutput("ui_split_Medical"),
       uiOutput("ui_name_Medical"),
-     checkboxInput("ld", "Load"),
+      uiOutput("ui_Medical_reports"),
 
       ## browse image
       fileInput(inputId = 'files',
-                label = 'Select an Image',
+                #value = '../extadata/reports/',
+                label = 'Select an Image to zoom in',
                 multiple = TRUE,
-                accept=c('image/png', 'image/jpeg'))
+                accept=c('image/png', 'image/jpeg')),
+
+      sliderInput("zoom_MedicalReport",
+                  "Scale in pixel:",
+                  min = 360,
+                  max = 1080,
+                  value = 360),
+
+      help_and_report(modal_title = "Medical Reports", fun_name = "medical",
+                      author = "Karim Mezhoud",
+                      help_file = inclRmd(file.path(
+                        getOption("radiant.path.dose"),"app/tools/help/medical.Rmd")))
 
 
     ),
-  #####  Main Page ###
-  mainPanel(
-    tagList(
-conditionalPanel("input.ld==true",
-  lapply(1:3, function(i){
+    #####  Main Page ###
+    mainPanel(
+      tabsetPanel(
+   tabPanel("Image",
+      tagList(
 
-    div(style="display:inline-block",
-        tags$a(imageOutput(paste0("display_image",i), width=360), href="https://www.google.com" ),
-        helpText( a("Click Here for the Source Code on Github!",
-                      href="https://github.com/Bohdan-Khomtchouk/Microscope",target="_blank"))
+      # tags$h5('Files path'),
+       # dataTableOutput('filesPath'),
+       # textOutput('directory'),
+
+        conditionalPanel("input.view_MedReportID==true",
+                         lapply(1:6, function(i){
+
+                           div(style="display:inline-block",
+                               tags$a(imageOutput(paste0("display_image",i),
+                                                  width= 330), # input$zoom_MedicalReport
+                                      href="https://www.google.com" )
+                               #helpText( a("Click Here for the Source Code on Github!",
+                               #           href="https://github.com/Bohdan-Khomtchouk/Microscope",target="_blank"))
+                           )
+                         })
         )
-  })
-)
- ,
+        ,
 
-  #### begin display image from file
-  tableOutput('files'),
-  uiOutput('images')
-  #### end display image from file
+        #### begin display image from file
+        #tableOutput('files'),
+        uiOutput('images')
+        #### end display image from file
 
-  # lapply(1:length(input$edata_vars), function(i) {
-  #   div(style="display:inline-block",
-  #       textInput(paste0(input$edata_vars[i],i), label = input$edata_vars[i], "", width= "60%")
-  #   )
-  # })
 
-#)
-
+      )
+   ),
+   tabPanel("Numeric")
+      )
     )
-  )
   )
 
 })
